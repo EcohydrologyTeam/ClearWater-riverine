4efda1965f7e0f0adb8a5e525f369112d27b91b2
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2022-09-21 17:26:35 -0500

    Delete early "sandbox" that was in `tests`
    `tests` will be reserved for unit tests and other automated testing code.
    Cleaning up old files that are no longer necessary, for #5. These files will always be available in this archived branch: https://github.com/EnvironmentalSystems/ClearWater-riverine/tree/archive_2022-July

--- tests/output_files/make_animated_gif.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Convert png files to jpeg files
-# for i in muncie_chloropleth*.png; do sips -s format jpeg -s formatOptions 70 "${i}" --out "${i%png}jpg"; done
-
-# Convert jpg files to animated GIF using ImageMagick's convert command
-# convert -delay 60 -loop 0 -size 640x480 muncie_chloropleth*.jpg muncie_chloropleth_maps.gif
-# convert -delay 60 -loop 0 -resize 1800x1350 muncie_chloropleth_045.jpg muncie_chloropleth_046.jpg muncie_chloropleth_047.jpg muncie_chloropleth_maps.gif
-convert -delay 60 -loop 0 -size 1800x1350 muncie_chloropleth*.jpg muncie_chloropleth_maps.gif
